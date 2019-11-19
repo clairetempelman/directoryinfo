45573e1aa9ca3c9736e5e826bbe7167bae4185e8
Claire Tempelman <32780669+clairetempelman@users.noreply.github.com>
2019-11-18 19:41:03 -0500

    Update directory.c

--- directory.c
+++ directory.c
@@ -9,14 +9,37 @@
 #include <dirent.h>
 #include <sys/types.h>
 
-int main(){
-  printf("Printing information about directory: .\n");
+int main(int argc, char *argv[]){
+  DIR *d;
+  char * dirname;
+  if (argc == 2){
+    dirname = argv[1];
+    d = opendir(dirname);
+    if (d == NULL){
+      printf("errno %d error: %s\n", errno, strerror(errno));
+    }
+  }
+  else{
+    char s[100];
+    printf("Input a path of a directory.\n");
+    fgets(s, 100, stdin);
+    char * checker;
+    if ((checker = strchr(s, '\n')) != NULL) {
+      *checker = '\0';
+    }
+    dirname = s;
+    d = opendir(dirname);
+    if (d == NULL){
+      printf("errno %d error: %s\n", errno, strerror(errno));
+    }
+  }
+  printf("Printing information about directory:\n");
   printf("Subdirectories: \n");
   long long size = 0;
-  DIR *d = opendir(".");
-  if (d == NULL){
-    printf("errno %d error: %s\n", errno, strerror(errno));
-  }
+  //d = opendir(".");
+  //if (d == NULL){
+  //  printf("errno %d error: %s\n", errno, strerror(errno));
+  //}
   struct dirent *ent;
   while((ent = readdir(d)) != NULL){
     if (ent->d_type == DT_DIR){
@@ -26,7 +49,7 @@ int main(){
   closedir(d);
   printf("\n");
   printf("Regular files: \n");
-  d = opendir(".");
+  d = opendir(dirname);
   if (d == NULL){
     printf("errno %d error: %s\n", errno, strerror(errno));
   }
